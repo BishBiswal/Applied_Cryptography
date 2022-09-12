f306edad263c88a896ec5b7808fbe9397e4eb16c
BishBiswal <112304474+BishBiswal@users.noreply.github.com>
2022-09-12 01:13:02 -0700

    Create ReserviorSampling398.java

---
+++ ReserviorSampling398.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    private int[] nums;
+    private Random rand;
+    
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rand = new Random();
+    }
+    
+    public int pick(int target) {
+        int n = this.nums.length;
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            // if nums[i] is equal to target, i is a potential candidate
+            // which needs to be chosen uniformly at random
+            if (this.nums[i] == target) {
+                // increment the count of total candidates
+                // available to be chosen uniformly at random
+                count++;
+                // we pick the current number with probability 1 / count (reservoir sampling)
+                if (rand.nextInt(count) == 0) {
+                    idx = i;
+                }
+            }
+        }
+        return idx;
+    }
+}
+
+//Loop 1: Rand could return 0 (100%)
+//Loop 2: Rand could return 0 or 1 (50%)
+//Loop 3: Rand could return 0 or 1 or 2 (33.33.. %)
